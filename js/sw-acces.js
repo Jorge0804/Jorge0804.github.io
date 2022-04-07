51da5922395bb529aa08ab8fc34c35237bc4a48d
Jorge Bernal <jorgebernal08041999@gmail.com>
2022-04-07 18:53:26 -0500

    clone

--- js/sw-acces.js
+++ js/sw-acces.js
@@ -1,9 +1,9 @@
 function actualizaCacheDinamico(dynamicCache, request, response){
     if(response.ok){
         //la respuesta tiene data y se debe almacaenar en cache
-        caches.open(dynamicCache).then(cache => {
+        return caches.open(dynamicCache).then(cache => {
             cache.put(request,response.clone() );
-            
+            return cache.clone();
         });
         return dynamicCache.clone();
     }else{//si no viene nda en la respuesta
