46bdecab8f5db37fc13b9bd2258dbfb210b00dc0
Jorge Bernal <jorgebernal08041999@gmail.com>
2022-04-07 18:36:38 -0500

    HOLA X2

--- sw.js
+++ sw.js
@@ -3,8 +3,17 @@ importScripts('js/sw-acces.js');
 const STATIC_CACHE = 'static-v1';
 const DYNAMIC_CACHE = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
-const APP_SHELL = ['/', 'index.html', 'css/style.css', 'images/favicon.ico', 
-'js/app.js', 'img/1.jpg', 'img/2.jpg', 'img/3.jpg', 'img/4.jpg'];
+const APP_SHELL = [
+    '/', 
+    './index.html', 
+    './css/style.css', 
+    './images/favicon.ico', 
+    './js/app.js', 
+    './img/1.jpg', 
+    './img/2.jpg', 
+    './img/3.jpg', 
+    './img/4.jpg'
+];
 const APP_SHELL_INMUTABLE = ['https://fonts.googleapis.com/css?family=Quicksand:300,400',
 'https://fonts.googleapis.com/css?family=Lato:400,300',
 'https://netdna.bootstrapcdn.com/font-awesome/3.1.1/css/font-awesome.css', 
