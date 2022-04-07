3d8b89c35c7c364de50c1af9fbdf0e50a6d4164e
Jorge Bernal <jorgebernal08041999@gmail.com>
2022-04-07 17:57:29 -0500

    once

--- js/app.js
+++ js/app.js
@@ -1,9 +1,7 @@
 var url = window.location.href; //obtenemos todo el url
 var pwaLocation = 'https://jorge0804.github.io/sw.js'; //path donde se encuentra el sw en GitHub
  
-if(navigator.serviceWorker){
-    if(url.includes('localhost')){
-        pwaLocation = '/sw.js';
-    }
-    navigator.serviceWorker.register(pwaLocation);
+if(url.includes('localhost')){
+    pwaLocation = '/sw.js';
 }
+navigator.serviceWorker.register(pwaLocation);
