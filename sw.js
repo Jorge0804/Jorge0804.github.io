d62a20db4ef8225c87f76a7234d7d3e0ff441732
Jorge Bernal <jorgebernal08041999@gmail.com>
2022-04-07 18:42:28 -0500

    trampa

--- sw.js
+++ sw.js
@@ -1,18 +1,20 @@
 importScripts('js/sw-acces.js');
 
+const myUrl = 'https://jorge0804.github.io/';
+
 const STATIC_CACHE = 'static-v1';
 const DYNAMIC_CACHE = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 const APP_SHELL = [
     '/', 
-    './index.html', 
-    './css/style.css', 
-    './images/favicon.ico', 
-    './js/app.js', 
-    './img/1.jpg', 
-    './img/2.jpg', 
-    './img/3.jpg', 
-    './img/4.jpg'
+    myUrl+'index.html', 
+    myUrl+'css/style.css', 
+    myUrl+'images/favicon.ico', 
+    myUrl+'js/app.js', 
+    myUrl+'images/1.jpg', 
+    myUrl+'images/2.jpg', 
+    myUrl+'img/3.jpg', 
+    myUrl+'img/4.jpg'
 ];
 const APP_SHELL_INMUTABLE = ['https://fonts.googleapis.com/css?family=Quicksand:300,400',
 'https://fonts.googleapis.com/css?family=Lato:400,300',
